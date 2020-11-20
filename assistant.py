3f033460941dde3f5ddd8bb13478e96adce85829
gabriela chavgova <chavgova88@gmail.com>
2020-11-21 01:29:15 +0200

    emoRec

--- assistant.py
+++ assistant.py
@@ -5,12 +5,36 @@ import wikipedia
 import pyttsx3 
 import speech_recognition as sr
 from spotify_local import SpotifyLocal
+import numpy as np
+
+######### EMOTION RECOGNITION
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+import librosa
+import os
+import pandas as pd
+import glob 
+from librosa import display
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
+
+json_file = open(r"C:\Users\ok\Documents\GABRIELA\ASSISTANT\MODELS\model_08_FEMALE_sigmoid_EmoMix.json", 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights(r"C:\Users\ok\Documents\GABRIELA\ASSISTANT\MODELS\Emotion_Voice_Detection_CNN_model_08_FEMALE_sigmoid_EmoMix.h5")
+print('=> THE FEMALE MODEL IS LOADED')
+######### EMOTION RECOGNITION - end
+
+audio_counter = 0
+wav_file = None
+speech_emos_list_values = None
 
 print("...")
 textSpeech = pyttsx3.init() 
 
 # Sets speed percent  
-textSpeech.setProperty('rate', 140) 
+textSpeech.setProperty('rate', 145) 
 # Set volume 0-1 
 textSpeech.setProperty('volume', 0.5)
 
@@ -28,7 +52,10 @@ speechRec = sr.Recognizer()
 def stt():
     with sr.Microphone() as source:
         print("I'm listening...")
-        audio_text = speechRec.listen(source)
+        audio_text = speechRec.listen(source)       
+        with open(f'your_file_{audio_counter}.wav', 'wb') as file:
+            wav_file = audio_text.get_wav_data()
+            file.write(wav_file)
         print("...")
     # recognize_() method will throw a request error if the API is unreachable, hence using exception handling
         try:
@@ -40,6 +67,75 @@ def stt():
             print("I did not get that, sir")
             tts("I did not get that sir")
             exit()   # TODO: think of something better -> retry?
+
+
+def extractFeatures(my_file, **kwargs):
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    X, sample_rate = librosa.core.load(my_file)
+    if chroma or contrast:
+        stft = np.abs(librosa.stft(X))
+    result = np.array([])
+    if mfcc:
+        mfccs = np.mean(librosa.feature.mfcc(y = X, sr = sample_rate, n_mfcc = 40).T, axis = 0)
+        result = np.hstack((result, mfccs))  # 40 values 
+    if chroma:
+        chroma = np.mean(librosa.feature.chroma_stft(S = stft, sr = sample_rate).T,axis = 0)
+        result = np.hstack((result, chroma))  # 12 values 
+    if mel:
+        mel = np.mean(librosa.feature.melspectrogram(X, sr = sample_rate).T,axis = 0)
+        result = np.hstack((result, mel))  # 128 values 
+    if contrast:
+        contrast = np.mean(librosa.feature.spectral_contrast(S = stft, sr = sample_rate).T,axis = 0)
+        result = np.hstack((result, contrast)) # 7 values 
+    if tonnetz:
+        tonnetz = np.mean(librosa.feature.tonnetz(y = librosa.effects.harmonic(X), sr = sample_rate).T,axis = 0)
+        result = np.hstack((result, tonnetz)) # 6 values 
+
+    return result
+
+def emoRec():
+    X, sample_rate = librosa.load(f'your_file_{audio_counter}.wav', res_type='kaiser_fast',duration=2.5,sr=22050*2,offset=0.5)
+    sample_rate = np.array(sample_rate)
+
+    demo_file = os.fspath(f'your_file_{audio_counter}.wav')
+    features_live  = extractFeatures(demo_file, mel = True, mfcc = True, contrast = True, chroma = True, tonnetz = True)
+    features_live = pd.DataFrame(data = features_live)
+    features_live = features_live.stack().to_frame().T
+
+    lb = LabelBinarizer()
+    features_live_2d = np.expand_dims(features_live, axis = 2)
+    live_preds = loaded_model.predict(features_live_2d, batch_size = 32, verbose = 1)
+    speech_emos_list_values = live_preds
+    print(live_preds)
+    #lb.fit(live_preds)
+    all = np.argsort(-live_preds, axis = 1)[:, :8]
+    for i in all:
+        print((lb.inverse_transform((i))))
+    best_n = np.argsort(-live_preds)[:, :3] # best_n = [* * *]
+
+    first_second = 0
+    second_third = 0
+
+    for n in best_n:
+        k = n
+        num = 1
+        for k in n:   
+            if num == 1: first_second = live_preds[0][k] / live_preds[0][n][1]  
+            elif num == 2: second_third = live_preds[0][k] / live_preds[0][n][2]
+            num += 1
+
+    for i in best_n:
+        print((lb.inverse_transform((i))))
+
+    print()
+    print('First/Second:')
+    print(first_second)
+    print('Second/Third:')
+    print(second_third)    
 
 def stopTalking():  #### 
     return ''
@@ -72,13 +168,26 @@ def takeNote():
 
 # TODO: clearNote()?
 # TODO: newNote()?
-##################     
+
+
+
+
+
+
+
+
+
+
+###############################################################################################################     
 
 tts('How can I help you madam?')
 
 while(True):
     
-    query = stt()  
+    query = stt() 
+    emoRec() 
+    audio_counter += 1
+
 
     if ('tell' or 'which' or 'what') and ('day' or 'today') in query.lower(): 
         print(tellDay())
